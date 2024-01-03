135a9d44f8fae62f9f0ab5a0a468e05e16b16910
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2024-01-03 12:37:34 +0200

    make build

---
+++ index.5e393ae0.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,l=0;l<e.length;l++){var r=e[l].querySelectorAll("td, th")[1],t=e[l].querySelectorAll("td, th")[e[l].cells.length-1];e[l].insertBefore(r.cloneNode(!0),t)}
+//# sourceMappingURL=index.5e393ae0.js.map
